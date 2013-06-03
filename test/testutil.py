4452ea67ec44085d272b8fd1aa5ca5c53d08faff
smcho <sm.cho@mac.com>
2013-06-03 10:44:47 -0500

    added unit test for util

---
+++ test/testutil.py
@@ -0,0 +1,32 @@
+import unittest
+import sys
+
+sys.path.append("../core")
+import util
+
+class TestUtil(unittest.TestCase):  
+    def setUp(self):
+        self.seq = range(10)
+    def test_getByteSize(self):
+        iList = [1, 8, 10, 20]
+        expected = [1, 1, 2, 3]
+        for i, val in enumerate(iList):
+            #print iList[i], expected[i]
+            self.assertTrue(util.getByteSize(val) == expected[i])
+    def test_byteArrayXor(self):
+        input1 = [255, 255]
+        input2 = [0, 255]
+        util.byteArrayXor(input1, input2)
+        self.assertTrue(input1[0] == 255)
+        self.assertTrue(input1[1] == 0)
+    def test_addAll(self):
+        a = ['x']
+        util.addAll(a, ['b','c'])
+        self.assertTrue(a, ['x','b','c'])
+    def test_removeAll(self):
+        a = {'a':10, 'b':20, 'c':30}
+        util.removeAll(a, ['b','c'])
+        self.assertTrue(a['a'], 10)
+        self.assertTrue(len(a), 1) # ['a'], 10)
+if __name__ == "__main__":
+    unittest.main()
